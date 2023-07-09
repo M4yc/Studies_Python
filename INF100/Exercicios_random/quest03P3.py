0cfdea8470c98b24ed97a11f05c6207704c0180c
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-07-09 13:13:51 -0300

    Update quest03P3.py

--- INF100/Exercicios_random/quest03P3.py
+++ INF100/Exercicios_random/quest03P3.py
@@ -1,7 +1,8 @@
 import numpy as np
 BIKES = np.empty((9,8),dtype=str)
 BIKES[8][7] = "R"
-
+for i in range(0,7):
+    BIKES[8][i] = 'E'
 for i in range(0,9):
     for j in range(0,8):
         if BIKES[i][j] == '':
