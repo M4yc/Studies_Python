328634f5568a6056395cccff2c01592432c92907
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-07-10 16:36:38 -0300

    Update quest03P3.py

--- INF100/Exercicios_random/quest03P3.py
+++ INF100/Exercicios_random/quest03P3.py
@@ -3,6 +3,8 @@ BIKES = np.empty((9,8),dtype=str)
 BIKES[8][7] = "R"
 for i in range(0,7):
     BIKES[8][i] = 'E'
+for i in range(0,8):
+    BIKES[i][2] = 'r'
 for i in range(0,9):
     for j in range(0,8):
         if BIKES[i][j] == '':
