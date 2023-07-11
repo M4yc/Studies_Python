9a755f20eed644f8d7c6281b648bb737165e72b4
Maycon Vinicius <62727540+M4yc@users.noreply.github.com>
2023-07-11 14:38:28 -0300

    Update Correcao_imagem.py

--- INF100/Exercicios_praticas/Correcao_imagem.py
+++ INF100/Exercicios_praticas/Correcao_imagem.py
@@ -1,14 +1,20 @@
 import imagens
-def inicImagem() :
-    #nome = input('Nome do arquivo: ')
-    nome = 'cores.png'
-    im = imagens.Imagem(nome)
-    # descobre o número de linhas da imagem
-    linha = im.altura
-    # descobre o número de colunas da imagem
-    coluna = im.largura
-
-    return im, linha, coluna
 
 
-inicImagem()
+#nome = input('Nome do arquivo: ')
+nome = 'cores.png'
+im = imagens.Imagem(nome)
+# descobre o número de linhas da imagem
+lin = im.altura
+# descobre o número de colunas da imagem
+col = im.largura
+
+for i in range(0,lin):
+    for j in range(0,col):
+        if im[i][j]==(0,255,0):
+            r, g ,b = im[i-1][j]
+            rb, gb, bb = im[i+1][j]
+            rm = (r + rb)//2
+            gm = (g + gb)//2
+            bm = (b + bb)//2
+            im[i][j]=(rm,gm,bm)
