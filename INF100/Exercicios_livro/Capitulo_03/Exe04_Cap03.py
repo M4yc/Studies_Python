a3d4d677d86224f08731c857d680b7bd71915f67
Maycon Vinicius <62727540+M4yc@users.noreply.github.com>
2023-07-13 14:57:05 -0300

    Update Exe04_Cap03.py

--- INF100/Exercicios_livro/Capitulo_03/Exe04_Cap03.py
+++ INF100/Exercicios_livro/Capitulo_03/Exe04_Cap03.py
@@ -11,8 +11,28 @@ if es == 5:
     C = float(input("Informe a temperatura: "))
     K = C + 273
     print(f"{C}° Celsius é {K}° Kelvin.")
+
+elif es == 4:
+    K = float(input("Informe a temperatura: "))
+    F = (K-273)*1.8+32
+    print(f"{K}° Kelvin é {F:.1f}° Fahrenheit..")
+
+elif es == 3:
+    F = float(input("Informe a temperatura: "))
+    K = (F-32)*5/9+273
+    print(f"{F}° Fahrenheit é {K:.1f}° Kelvin.")
+
+elif es == 2:
+    F = float(input("Informe a temperatura: "))
+    C = (F -32)/1.8
+    print(f"{F}° Fahrenheit é {C}° Celsius.")
+
 elif es == 1:
     K = float(input("Informe a temperatura: "))
     C = 273 - K
     print(f"{K}° Kelvin é {C}° Celsius.")
-    print("%f° Kelvin é %f° Celsius."%(K,C))
+
+elif es == 0:
+    C = float(input("Informe a temperatura: "))
+    F = 1.8 * C + 32
+    print(f"{C}° Celsius é {F:.1f}° Fahrenheit..")
