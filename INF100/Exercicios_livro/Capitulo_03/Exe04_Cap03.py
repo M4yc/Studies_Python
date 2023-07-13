44d54e9938dbe14476c1f13f6b8a46b925595e30
Maycon Vinicius <62727540+M4yc@users.noreply.github.com>
2023-07-13 14:40:39 -0300

    Update Exe04_Cap03.py

--- INF100/Exercicios_livro/Capitulo_03/Exe04_Cap03.py
+++ INF100/Exercicios_livro/Capitulo_03/Exe04_Cap03.py
@@ -1,1 +1,18 @@
 #programa de conversão de temperatura
+print("[0] De Celsius para Fahrenheit.")
+print("[1] De Celsius para Kelvin.")
+print("[2] De Fahrenheit para Celsius.")
+print("[3] De Fahrenheit para Kelvin.")
+print("[4] De Kelvin para Fahrenheit")
+print("[5] De Kelvin para Celsius")
+
+es = int(input("Qual opção você esolhe: "))
+if es == 5:
+    C = float(input("Informe a temperatura: "))
+    K = C + 273
+    print(f"{C}° Celsius é {K}° Kelvin.")
+elif es == 1:
+    K = float(input("Informe a temperatura: "))
+    C = 273 - K
+    print(f"{K}° Kelvin é {C}° Celsius.")
+    print("%f° Kelvin é %f° Celsius."%(K,C))
