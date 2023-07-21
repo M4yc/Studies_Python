a44a33ca85def4a52b52eb6dc3a75daad700754b
Maycon Vinicius <62727540+M4yc@users.noreply.github.com>
2023-07-21 10:28:45 -0300

    ExeCriado
    Executável criado agora falta personalizar o programa

--- INF100/Exercicios_random/Macro.py
+++ INF100/Exercicios_random/Macro.py
@@ -1,25 +1,33 @@
 import pyautogui
 import keyboard
-
-
+import tkinter as tk
+from threading import Thread
 
 def mover_e_clicar(x, y):
     pyautogui.moveTo(x, y, duration=0.5)  # Move o cursor para a posição (x, y)
     pyautogui.click()  # Executa o clique esquerdo
 
-def main():
+def executar_macro():
     x_pos1, y_pos1 = 1123, 1071
     x_pos2, y_pos2 = 959, 799
 
-    print("Pressione Control + 2 para parar o loop.")
-    while True:
-        # Executa a macro
+    while not keyboard.is_pressed('ctrl') and not keyboard.is_pressed('2'):
         mover_e_clicar(x_pos1, y_pos1)
         mover_e_clicar(x_pos2, y_pos2)
 
-        # Verifica se a tecla Control + 2 foi pressionada para parar o loop
-        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('2'):
-            print("Loop parado.")
-            break
+def iniciar_thread():
+    t = Thread(target=executar_macro)
+    t.start()
 
-main()
+# Interface Gráfica
+root = tk.Tk()
+root.title("Executar Macro")
+root.geometry("300x100")
+
+label = tk.Label(root, text="Pressione Ctrl+2 para parar o loop")
+label.pack()
+
+botao_iniciar = tk.Button(root, text="Iniciar Macro", command=iniciar_thread)
+botao_iniciar.pack()
+
+root.mainloop()
