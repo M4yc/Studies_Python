85e6bcb5f9e124b26fd0f5d77919c5b33dd6ed4a
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-09-05 09:06:51 -0300

    Update 05-09.py

--- INF101/Exe_Aula_Teo/05-09.py
+++ INF101/Exe_Aula_Teo/05-09.py
@@ -5,7 +5,8 @@ l=[]
 l=[random.randint(0,100)for i in range(6)]
 
 #l=[1,2,3,4,5]
-#l.insert(2,8)
+#
+l.insert(2,8)
 
 for i in range(len(l)):
     print(l[i],end=' ')
@@ -13,4 +14,9 @@ for i in range(len(l)):
 
 
 print("\n")
-print(max(l))
+print(f"Maximo: {max(l)}")
+#print(max(l))
+print(f"Pop: {l.pop()}")
+#print(l.pop())
+print(f"Lista depois do Pop:{l}")
+
