aa079b807d60952aefac57a9cff5d71401f0364b
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-09-12 08:50:12 -0300

    Update 12-09.py

---
+++ INF101/Aula_teorica/12-09.py
@@ -0,0 +1,9 @@
+import math
+import numpy
+import random
+
+quadrados = [math.sqrt(x) for x in range(10)]
+
+for i in quadrados:
+    print(f"Imprime lista com o quadrado dos números de 0 a 9:{i:.2f}")
+    
