af79f762cae895a635c5c6d098a532dc8891b180
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-09-14 14:41:05 -0300

    Create P4_14-09.py

---
+++ INF101/exe_random/P4_14-09.py
@@ -0,0 +1,27 @@
+import random
+def main():
+    #l=[22,1,4,2,5,65,7,85,9]
+    #l=[36]
+    l=[14,54,24,75,11,77,23,11]
+    #l=[]
+    #l=[random.randint(0,100) for i in range(9)]
+    print("Lista Original: ",l)
+
+    print("Segundo Menor valor da lista: ",segundoMenor(l))
+def segundoMenor(l):
+    lista = l
+    menor = l[0]
+    menor2 = l[0]
+    if len(l) > 1:
+        for i in range(len(l)):
+            for j in range(len(l)):
+                if l[i] < menor:
+                    menor = l[i]
+                if l[j] < menor2 and l[j] > menor:
+                    menor2 = l[j]
+                elif l[i] == menor:
+                    menor2 = l[j]
+        return menor2
+    else:
+        return None
+main()
