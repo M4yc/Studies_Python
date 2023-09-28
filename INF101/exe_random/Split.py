1dea1243b365bd12bec9c6fcd767e67dedc49dc9
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-09-28 08:16:22 -0300

    Create Split.py

---
+++ INF101/exe_random/Split.py
@@ -0,0 +1,10 @@
+# Exemplo de funcionamento da método split().
+
+frase = 'michel melo da silva'
+print(frase)
+dados = frase.split(' ')
+# 'michel melo da silva' -> ['michel', 'melo', 'da', 'silva']
+print(dados[0])
+print(dados[1])
+print(dados[2])
+print(dados[3])
