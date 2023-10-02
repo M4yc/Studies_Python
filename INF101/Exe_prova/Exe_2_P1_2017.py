789c4f29bb75702b3a15f88b46ff3818d865fc51
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-02 17:03:57 -0300

    Update Exe_2_P1_2017.py

--- INF101/Exe_prova/Exe_2_P1_2017.py
+++ INF101/Exe_prova/Exe_2_P1_2017.py
@@ -1,5 +1,13 @@
 def addOne(d,m,a):
     lista = ([d],[m],[a])
+    if d < dias_mes(m,a):
+        return (d + 1, m, a)
+    else:
+        if m < 12:
+            return (1, m + 1, a)
+        else:
+            return (1, 1, a + 1)
+
 
 def dias_mes(m,a):
     if m == 2 and bissexto(a) == True:
@@ -20,3 +28,8 @@ def bissexto(a):
     else:
         #print("Não é bissexto")
         return False
+
+data_at = (28,2,2022)
+nova_data = addOne(*data_at)
+print(f"Data atual: {data_at}")
+print(f"Nova data: {nova_data}")
