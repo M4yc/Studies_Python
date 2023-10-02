cc8d3a13f9adacbad16a5730ebdd075fc1857685
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-02 17:37:38 -0300

    Update Exe_3_P1_2017.py

--- INF101/Exe_prova/Exe_3_P1_2017.py
+++ INF101/Exe_prova/Exe_3_P1_2017.py
@@ -1,2 +1,14 @@
-
-peso = [5,7,5,7,5]
+def calc(matr):
+    matricula = [3,4,5,2,1]
+    peso = [5,7,5,7,5]
+    soma = 0
+    for i in range(0,len(matricula)):
+        soma = soma + int(matr[i]*peso[i])
+    resto = soma % 11
+    if resto < 2:
+        dig = str(0)
+    else:
+        dig = str(11 - resto)
+    return dig
+def matrval(matr):
+    return matr[5] == calc(matr)
