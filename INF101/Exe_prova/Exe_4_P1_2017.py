8e223b320b439d52ba96f6962589d3bb6ec17371
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-02 16:32:42 -0300

    Update Exe_4_P1_2017.py

--- INF101/Exe_prova/Exe_4_P1_2017.py
+++ INF101/Exe_prova/Exe_4_P1_2017.py
@@ -1,9 +1,12 @@
 def calcular_media(lista_de_tuplas):
-    
     if not lista_de_tuplas:
         return 0.0
 
-    soma_das_notas = sum(nota for matricula, nota in lista_de_tuplas)
+    soma_das_notas = 0
+    #soma_das_notas = sum(nota for matricula, nota in lista_de_tuplas)
+
+    for matricula, nota in lista_de_tuplas:
+        soma_das_notas += nota
     numero_de_alunos = len(lista_de_tuplas)
 
     media = soma_das_notas / numero_de_alunos
