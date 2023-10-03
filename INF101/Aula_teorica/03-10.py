bf3b08ad12bffcdbf8c856322ae2b3f262e0ab02
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-03 09:24:32 -0300

    Update 03-10.py

--- INF101/Aula_teorica/03-10.py
+++ INF101/Aula_teorica/03-10.py
@@ -3,21 +3,16 @@ notas = {"113683": ("Maycon", 21, "M"),
          "113685": ("Josue", 15, "M"),
          "113686": ("Isabella", 17, "F")}
 
-#cont = 0
-
-#for matricula, (nome, idade, sexo) in notas.items():
-    #if idade > 18:
-        #cont += 1
-
-#print("Maiores de 18:", cont)
-
-notas = ["113685"][1] = 18
-
 def verIdade(notas):
     cont = 0
     for matricula, (nome, idade, sexo) in notas.items():
-        if idade > 18:
+        if idade >= 18:
             cont += 1
 
     return cont
 
+print(verIdade(notas))
+
+notas["113685"] = ("Josue", 18, "M")
+
+print(verIdade(notas))
