e02a4483f4f2a8bec67946adc62f2177b24d2d53
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-03 08:24:52 -0300

    Update 03-10.py

---
+++ INF101/Aula_teorica/03-10.py
@@ -0,0 +1,12 @@
+D = { "Alface" : 0.45,
+      "Batata" : 1.20,
+      "Tomate" : 2.30,
+      "Feijão" : 5.50 }
+
+Dici = {"Maycon" : (20, 30)}
+
+#print(f"Preço do Feijão R${D['Feijão']:.2f}")
+#print(D)
+#D["Cebola"] = 1.35
+#print(D)
+print(Dici)
