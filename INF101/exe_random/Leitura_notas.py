bd08a368519e19a4d4d42483cff3cf21373a3c7c
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-03 11:38:33 -0300

    Update Leitura_notas.py

--- INF101/exe_random/Leitura_notas.py
+++ INF101/exe_random/Leitura_notas.py
@@ -1,25 +1,26 @@
-def abraArq(nomeArq):
-    print()
+def abraArq():
+    notas = {}
+    arq = open("notas.txt", 'r')
+    arq.readline()
 
-notas = {}
-arq = open("notas.txt", 'r')
-arq.readline()
+    linha =arq.readline().rstrip('\n')
+    while linha != '':
+        dados = linha.split('; ')
+        matr = int(dados[0])
+        nome = str(dados[1])
+        idade = int(dados[2])
+        p1 = float(dados[3])
+        p2 = float(dados[4])
+        p3 = float(dados[5])
+        notas[matr]= (nome, idade, p1, p2, p3)
+        linha = arq.readline().rstrip('\n')
+    arq.close()
 
-linha =arq.readline().rstrip('\n')
-while linha != '':
-    dados = linha.split('; ')
-    matr = int(dados[0])
-    nome = str(dados[1])
-    idade = int(dados[2])
-    p1 = float(dados[3])
-    p2 = float(dados[4])
-    p3 = float(dados[5])
-    notas[matr]= (nome, idade, p1, p2, p3)
-    linha = arq.readline().rstrip('\n')
-arq.close()
+    return notas
+
+notas = abraArq()
 
 notaf = 0
-
 for matricula, (nome, idade, p1, p2, p3) in notas.items():
     notaf = p1 + p2 + p3
     notas[matricula] = (nome, idade, p1, p2, p3,notaf)
