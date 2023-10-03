23dcaf5ca7435eed88fa61ce55e0efe2afb8771e
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-03 17:27:53 -0300

    Update estoque.py

---
+++ INF101/exe_random/estoque.py
@@ -0,0 +1,7 @@
+estoque = { 'Tomate' : [1000, 2.30],
+            'Cenoura' : [500, 1.50],
+            'Batata': [1500, 1.30],
+            'Couve' : [300, 1.00],
+            'Feijao' : [500, 6.00]}
+
+vendas = [('Tomate', 5), ('Batata', 100), ('Cenoura', 40), ('Couve', 20), ('Feijao', 80)]
