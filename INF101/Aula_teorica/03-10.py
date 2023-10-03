f3e1e08aec641b7ff05a7afdd71f93cdc29c8ed4
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-03 08:31:35 -0300

    Update 03-10.py

--- INF101/Aula_teorica/03-10.py
+++ INF101/Aula_teorica/03-10.py
@@ -3,10 +3,14 @@ D = { "Alface" : 0.45,
       "Tomate" : 2.30,
       "Feijão" : 5.50 }
 
-Dici = {"Maycon" : (20, 30)}
+Dici = {"Maycon" : (20, 30,1.15),
+        "Mariana" : (10,15,1.8)}
 
 #print(f"Preço do Feijão R${D['Feijão']:.2f}")
 #print(D)
 #D["Cebola"] = 1.35
 #print(D)
-print(Dici)
+print(Dici["Maycon"][2])
+
+if Dici["Maycon"][1] == 30:
+    print("Certo")
