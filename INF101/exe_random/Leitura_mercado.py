39964c293b719118a0fb55d61cd968035d6eea51
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-04 15:23:51 -0300

    Update Leitura_mercado.py

--- INF101/exe_random/Leitura_mercado.py
+++ INF101/exe_random/Leitura_mercado.py
@@ -1,51 +1,44 @@
 def abrirArq1():
     estoque = {}
-    arq = open("mercado.txt", "r")
-    arq.readline()
-    #with open("mercado.txt", "r") as arquivo:
+    with open("mercado.txt", "r") as arq:
+        arq.readline()  # Pule a primeira linha do arquivo
 
-    linha = arq.readline().rstrip('\n')
-    while linha != '':
-        dados = linha.split('; ')
-        produto = str(dados[0])
-        qnt = int(dados[1])
-        mod = str(dados[2])
-        estoque[produto]=[qnt, mod]
-        linha = arq.readline().rstrip('\n')
+        for linha in arq:
+            dados = linha.rstrip('\n').split('; ')
+            produto = str(dados[0])
+            qnt = int(dados[1])
+            mod = str(dados[2])
+            estoque[produto] = [qnt, mod]
 
-    arq.close()
-
-    return  estoque
-
+    return estoque
 
 def abrirArq2():
     precos = {}
-    arq = open("preco.txt", "r")
-    arq.readline()
-    # with open("mercado.txt", "r") as arquivo:
+    with open("preco.txt", "r") as arq:
+        arq.readline()  # Pule a primeira linha do arquivo
 
-    linha = arq.readline().rstrip('\n')
-    while linha != '':
-        dados = linha.split('; ')
-        produto = str(dados[0])
-        qnt = int(dados[1])
-        mod = str(dados[2])
-        price = float(dados[3])
-        precos[produto] = [qnt, mod, price]
-        linha = arq.readline().rstrip('\n')
-
-    arq.close()
+        for linha in arq:
+            dados = linha.rstrip('\n').split('; ')
+            produto = str(dados[0])
+            qnt = int(dados[1])
+            mod = str(dados[2])
+            price = float(dados[3])
+            precos[produto] = [qnt, mod, price]
 
     return precos
 
-estoque  = abrirArq1()
-
+estoque = abrirArq1()
 preco = abrirArq2()
 
 extrato = {}
 
-for produto1, (qnt1,mod1) in estoque.items():
-    for produto2, (qnt2, mod2,price) in preco.items():
-        extrato[produto1] = (qnt2, mod2,price*qnt1)
+for produto_estoque, (qnt_estoque, mod_estoque) in estoque.items():
+    for produto_preco, (qnt_preco, mod_preco, price) in preco.items():
+        extrato[produto_preco] = (qnt_estoque, mod_preco, price * qnt_estoque)
 
-print(extrato)
+pd_desj = 'Farinha'
+try:
+    qnt, mod, valor = extrato[pd_desj]
+    print(f'Produto: {pd_desj:12s}     Quantidade: {qnt}     Valor: R$ {valor:.2f}')
+except KeyError:
+    print(f'Produto: "{pd_desj}" não encontrado no extrato.')
