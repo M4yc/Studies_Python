488b51bc344151b9c8844c3fb5f5ac656893b4ac
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-04 20:06:41 -0300

    Update exe_P07_acho.py

--- INF101/Exe_Aula_Teo/exe_P07_acho.py
+++ INF101/Exe_Aula_Teo/exe_P07_acho.py
@@ -2,8 +2,8 @@ def main():
     alunos = leiaAlunos('let374.dat')
     reprov = verifiqueReprovados(alunos)
     #Impressão relatorio
-    print("\n *** Alunos Reprovados na Disciplina LET374***\n"
-          "Matricula                Nome        Curso       Nota        Faltas")
+    print("\n              *** Alunos Reprovados na Disciplina LET374 ***\n"
+          "Matricula           Nome                  Curso                  Nota  Faltas")
     for i in range(len(reprov)):
         print(f'%7d    %-25s   %-24s   %1d   %4d' % (reprov[i][0], reprov[i][1], reprov[i][2], reprov[i][3], reprov[i][4]))
 
