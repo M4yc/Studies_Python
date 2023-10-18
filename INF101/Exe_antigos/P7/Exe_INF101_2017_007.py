e888217cb15e1770380e8f7305ce99c3b2882827
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-18 20:46:50 -0300

    exe07

---
+++ INF101/Exe_antigos/P7/Exe_INF101_2017_007.py
@@ -0,0 +1,3 @@
+# Faz um relatório dos salários brutos auferidos pelos funcionários de
+# uma empresa. Os registros dos funcionários são lidos do arquivo
+# 'funcionarios.csv' e as horas trabalhadas, do arquivo 'horas_trab.dat'.
