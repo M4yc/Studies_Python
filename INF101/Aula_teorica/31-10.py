5f1d585aa4a7528fa0d14188d62c40578e190970
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-31 09:25:44 -0300

    televisor

--- INF101/Aula_teorica/31-10.py
+++ INF101/Aula_teorica/31-10.py
@@ -2,10 +2,12 @@
 #import televisor
 from televisor import Televisor
 
-tv = Televisor(1, 99)
+tv = Televisor(1, 99, 4, 30)
 for x in range(120):
     tv.mudaCanalParaCima()
 print(tv.canal)
 for x in range(120):
     tv.mudaCanalParaBaixo()
 print(tv.canal)
+tv.aumentaVolume()
+print(tv.volume)
--- INF101/Aula_teorica/televisor.py
+++ INF101/Aula_teorica/televisor.py
@@ -1,14 +1,21 @@
 class Televisor:
-    def __init__(self, min, max):
+    def __init__(self, min, max, c, vol):
         self.marca = None
         self.tamanho = 0
         self.ligado = False
-        self.canal = 2
+        self.canal = c
         self.cmin = min
         self.cmax = max
+        self.volume = vol
     def mudaCanalParaCima(self):
         if self.canal + 1 <= self.cmax:
             self.canal += 1
     def mudaCanalParaBaixo(self):
         if self.canal - 1 >= self.cmin:
             self.canal -= 1
+    def aumentaVolume(self):
+        if self.volume + 1 <= 100:
+            self.volume += 1
+    def diminuiVolume(self):
+        if self.volume - 1 >= 0:
+            self.volume -= 1
