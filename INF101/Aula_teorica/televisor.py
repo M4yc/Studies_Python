05ea197fc26d09ca077ffa23d0ac0b759c285a43
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-10-31 09:33:18 -0300

    Update televisor.py

--- INF101/Aula_teorica/televisor.py
+++ INF101/Aula_teorica/televisor.py
@@ -10,9 +10,15 @@ class Televisor:
     def mudaCanalParaCima(self):
         if self.canal + 1 <= self.cmax:
             self.canal += 1
+        else:
+            self.canal = self.cmin
+            
     def mudaCanalParaBaixo(self):
         if self.canal - 1 >= self.cmin:
             self.canal -= 1
+        else:
+            self.canal = self.cmax
+
     def aumentaVolume(self):
         if self.volume + 1 <= 100:
             self.volume += 1
