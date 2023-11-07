58c57f154a2a48d009524a2a779f627c64112e59
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-11-07 08:33:56 -0300

    1

--- INF101/Exe_Aula_Teo/Banco de varejo/Bancos de Varejo.py
+++ INF101/Exe_Aula_Teo/Banco de varejo/Bancos de Varejo.py
@@ -1,4 +1,4 @@
-from bancos import Cliente
+from bancos import Cliente, Conta
 
 cliente1 = Cliente("Ana Lopes", "3123-4567")
 cliente2 = Cliente("Rui Castro", "3234-6789")
@@ -6,3 +6,11 @@ cliente2 = Cliente("Rui Castro", "3234-6789")
 print(cliente1.nome)
 print(cliente2.nome)
 print(cliente1.telefone)
+
+conta.resumo()
+conta.saque(1000.00)
+conta.resumo()
+conta.saque(50.00)
+conta.resumo()
+conta.deposito(234.56)
+conta.resumo()
