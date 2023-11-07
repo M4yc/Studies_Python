9779ea9078ef1be57f5438ff8ea886ec4c77405b
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-11-07 08:26:58 -0300

    Delete Bancos de Varejo.py

--- INF101/Exe_Aula_Teo/Bancos de Varejo.py
+++
@@ -1,8 +0,0 @@
-from bancos import Cliente
-
-cliente1 = Cliente("Ana Lopes", "3123-4567")
-cliente2 = Cliente("Rui Castro", "3234-6789")
-
-print(cliente1.nome)
-print(cliente2.nome)
-print(cliente1.telefone)
