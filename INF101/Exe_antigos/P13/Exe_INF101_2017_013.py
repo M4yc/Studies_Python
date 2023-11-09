d0bff4e7658b89a69d80e75409c084ce96aaac8b
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-11-08 22:39:21 -0300

    Create Exe_INF101_2017_013.py

---
+++ INF101/Exe_antigos/P13/Exe_INF101_2017_013.py
@@ -0,0 +1,57 @@
+
+class Cliente:
+    def __init__(self, nome, telefone):
+        self.nome = nome
+        self.telefone = telefone
+
+class Conta:
+    def __init__(self, clientes, numero, saldo=0.00):
+        self.clientes = clientes
+        self.numero = numero
+        self.saldo = saldo
+        self.operacoes = []
+        self.deposito(saldo)
+
+    def deposito(self, valor):
+        self.saldo += valor
+        self.operacoes.append(("DEPÓSITO", valor))
+
+    def extrato(self):
+        print("\nExtratodaCC nº %s\n" % self.numero)
+        for op in self.operacoes:
+            print("%10s %10.2f\n" % (op[0], op[1]))
+        print("SALDO%10.2f" % self.saldo)
+
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            self.operacoes.append(("SAQUE", valor))
+        else:
+            print('Cc nº {} com saldo insuficiente; SAQUE NÃO REALIZADO'. \
+                  format(self.numero))
+
+    def resumo(self):
+        print("\nCc nº %s Saldo: %10.2f" %
+              (self.numero, self.saldo))
+
+
+##############################################################
+def main():
+    cliente1 = Cliente("João Silva", "3234-7890")
+    cliente2 = Cliente("Maria Silva", "3234-7890")
+    cliente3 = Cliente("José Vargas", "2567-0987")
+    conta1 = Conta([cliente1, cliente2], 1, 1000.00)
+    conta2 = Conta([cliente3], 2, 500.00)
+    conta1.saque(50.00)
+    conta2.deposito(300.00)
+    conta1.saque(190.00)
+    conta2.deposito(95.15)
+    conta2.saque(256.71)
+    conta1.resumo()
+    conta2.resumo()
+    conta1.extrato()
+    conta2.extrato()
+
+###################################################################
+
+main()
