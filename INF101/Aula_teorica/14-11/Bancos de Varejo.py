cd3cff0d13de0c790c00cc8205618da2ef704016
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-11-14 08:18:01 -0300

    Create Bancos de Varejo.py

---
+++ INF101/Aula_teorica/14-11/Bancos de Varejo.py
@@ -0,0 +1,24 @@
+from bancos import Cliente, Conta, Banco
+
+cliente1 = Cliente("João Silva", "3456-7890")
+cliente2 = Cliente("Maria Silva", "3456-7890")
+cliente3 = Cliente("José Vargas", "2351-1809")
+
+contaJM = Conta([cliente1, cliente2], '76534', 100.00)
+contaJ= Conta([cliente3], '80297', 10.00)
+
+banco1 = Banco("Tatu")
+banco1.abreConta(contaJM)
+banco1.abreConta(contaJ)
+banco1.listaContas()
+
+# print( type(banco1.contas[0].clientes[0].nome) )
+print( banco1.contas[0].clientes[0].nome )
+print( banco1.contas[0].clientes[1].nome )
+# print( banco1.contas[0].clientes[2].nome )
+print( banco1.contas[1].clientes[0].nome )
+
+contaJJ = Conta([cliente1,cliente3], '00001', 500)
+
+banco1.abreConta(contaJJ)
+banco1.listaContas()
