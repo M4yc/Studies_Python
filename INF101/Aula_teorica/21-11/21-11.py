34a0672a8c225b428b91349017dc1706ddd81410
Maycon Vinicius <mayconvbatista84@gmail.com>
2023-11-21 08:37:54 -0300

    Update 21-11.py

--- INF101/Aula_teorica/21-11/21-11.py
+++ INF101/Aula_teorica/21-11/21-11.py
@@ -1,1 +1,20 @@
 #Aula de Try-execpt
+def abrirArq(caminho):
+    print()
+try:
+    caminho = "funcionarios.csv"
+    #arquivo = open(caminho, 'r')
+
+    with open(caminho, 'r') as arquivo:
+        linhas = [l.rstrip() for l in arquivo.readline()]
+        dados = []
+        for l in linhas:
+            d = tuple(l.split(','))
+            dados.append(d)
+
+    print(dados)
+    #arquivo.close()
+except FileExistsError:
+    print('Arquivo não encontrado.')
+except FileNotFoundError:
+    print()
